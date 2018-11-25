070d91fda033f148a6db60ebced91423bbe4974e
ABabiker98 <40284177@napier.live.ac.uk>
2018-11-25 21:49:37 +0000

    Final Submission
    The Final working version of the project

--- sourcecode/cw2.py
+++ sourcecode/cw2.py
@@ -12,10 +12,10 @@ def creator():
 @app.route('/archive/')
 def archive():
     return render_template('archive.html')
-@app.route('/login/')
+@app.route('/login')
 def loginpage():
     return render_template('login.html')
-@app.route('/register')
+@app.route('/login/register')
 def register():
     return render_template('register.html')
 
@@ -27,25 +27,25 @@ def page_not_found(error):
 def internal_server_error(e):
     return render_template('error505.html'), 500S
 
-@app.route('/login', methods=['POST'])
+@app.route('/login/', methods=['POST'])
 def do_admin_login():
-POST_USERNAME = str(request.form['username'])
-POST_PASSWORD = str(request.form['password'])
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
 
-Session = sessionmaker(bind=engine)
-s = Session()
-query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
 result = query.first()
-     if result:
-    session['logged_in'] = True
-else:
-    flash('wrong password!')
-return login()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+        return login()
 
 @app.route("/logout")
 def logout():
-session['logged_in'] = False
-return home()
+    session['logged_in'] = False
+    return home()
 
 if __name__ == "__cw2__":
     app.run(host=0.0.0.0 , debug=True)
